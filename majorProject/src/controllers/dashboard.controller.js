86a58e576913910ca961b5b41a6b8c11c820b7ba
PragyaPandey96 <ppragya651@gmail.com>
2026-06-18 19:38:42 +0530

    comment controller

--- majorProject/src/controllers/dashboard.controller.js
+++ majorProject/src/controllers/dashboard.controller.js
@@ -9,11 +9,14 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 
 const getChannelStates = asyncHandler(async(req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+   const channelId = req.user._id
 })
 
 const getChannelVideos = asyncHandler(async(req, res) => {
     // TODO: Get all the videos uploaded by the channel
-})
+    const totalVideos = await Video.countDocuments({
+        owner: channelId
+});
 
 
 export {
