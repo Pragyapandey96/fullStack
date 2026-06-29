13eb83ddb4ecf120c5623c19012e2bcf5c0497b3
PragyaPandey96 <ppragya651@gmail.com>
2026-06-29 11:28:40 +0530

    ui-project page1 complete

---
+++ frontend/ui-project/src/components/Section1/HeroText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div>
+        <h3 className='text-6xl font-bold mb-7'>Prospective <br /> <span>Customer</span> <br/>Segmentation</h3>
+       <p className='text-xl font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus iure voluptate fuga pariatur fugiat voluptatum officia ipsum tempora laborum error!</p>
+      </div>
+  )
+}
+
+export default HeroText
---
+++ frontend/ui-project/src/components/Section2/Section2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section2 = () => {
+  return (
+    <div className='h-screen w-full bg-blue-900'>
+      <h2>section2</h2>
+    </div>
+  )
+}
+
+export default Section2
