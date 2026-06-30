818abd308a7f8c75d4869f48dfdbeb5486a8b24e
PragyaPandey96 <ppragya651@gmail.com>
2026-06-30 21:39:58 +0530

    notes-app

--- frontend/tailwind-props/src/App.jsx
+++ frontend/tailwind-props/src/App.jsx
@@ -1,24 +1,19 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const App = () => {
-  const [num, setNum] = useState(0)
-
-  function increaseNum () {
-    setNum(num + 1)
-  } 
-  function decreaseNum () {
-    setNum (num - 1 )
+  const submitHandler = (e) => {
+   e.preventDefault()
+    console.log("form submitted");
+    
   }
-  function jumpNum () {
-    setNum (num + 5)
-  }
-
   return (
     <div>
-      <h1>{num}</h1>
-      <button onClick={increaseNum}>Increase</button>
-      <button onClick={decreaseNum}>Decrease</button>
-      <button onClick={jumpNum}>jump by 5</button>
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }}>
+        <input type="text" placeholder='Enter your name' />
+        <button>submit</button>
+      </form>
     </div>
   )
 }
