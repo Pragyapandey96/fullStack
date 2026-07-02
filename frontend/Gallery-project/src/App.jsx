15bcfb40db890349bf6efded858cc0d58fd62be7
PragyaPandey96 <ppragya651@gmail.com>
2026-07-02 17:36:24 +0530

    gallery project done

--- frontend/Gallery-project/src/App.jsx
+++ frontend/Gallery-project/src/App.jsx
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+import Card from './components/Card'
 
 const App = () => {
+  
+  const [userData, setUserData] = useState([]);
+
+  const [index, setIndex] = useState(1)
+
+  const getData = async () => {
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=30`)
+    setUserData(response.data);
+  }
+
+  useEffect(function(){
+    getData()
+  }, [index])
+
+  let printUserData = <h3 className='text-gray-400 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>Loading...</h3>
+
+  if(userData.length>0){
+    printUserData = userData.map(function(elem, idx){
+     
+      return <div key={idx}>
+       <Card elem={elem} />
+      </div>
+    })
+  }
+
   return (
-    <div>
-      <div className='bg-black h-screen text-white'>get data</div>
-    </div>
+      <div className='bg-black overflow-auto h-screen text-white'>
+        <div className='flex flex-wrap gap-4 p-2'>
+          {printUserData}
+        </div>
+        <div className='flex justify-center gap-6 items-center'>
+          <button 
+          style={{ opacity: index == 1 ? 0.6 : 1}}
+          className='bg-amber-400 text-sm cursor-pointer text-black rounded px-4 py-2 font-semibold'
+          onClick={()=>{
+             if(index>1){
+              setIndex(index-1)
+              setUserData([])
+             }
+          }}
+          >
+            Prev
+            </button>
+
+            <h4>Page {index} </h4>
+          <button className='bg-amber-400 text-sm cursor-pointer text-black rounded px-4 py-2 font-semibold'
+          onClick={()=>{
+            setUserData([])
+             setIndex(index+1)
+          }}
+          >
+            Next
+            </button>
+        </div>
+      </div>
+    
   )
 }
 
