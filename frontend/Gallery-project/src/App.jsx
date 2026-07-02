221a39f2dc83f83ea1f792fbac03cc37a8071b0c
PragyaPandey96 <ppragya651@gmail.com>
2026-07-02 12:56:12 +0530

    gallery-project

---
+++ frontend/Gallery-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='bg-black h-screen text-white'>get data</div>
+    </div>
+  )
+}
+
+export default App
