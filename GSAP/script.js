a199da49222f0034d13648c8322e78904a5fcfa0
PragyaPandey96 <ppragya651@gmail.com>
2026-09-04 10:45:19 +0530

    scroll Trigger

--- GSAP/script.js
+++ GSAP/script.js
@@ -1,28 +1,10 @@
-gsap.to("#box1", {
+gsap.to("#box", {
     x: 1000,
-    duration: 2,
+    duration: 1,
     delay: 1,
     rotate:360,
-    backgroundColor: "green",
-    borderRadius: "50%",
-   
+    repeat:-1,
+   yoyo: true,
 })
 
-gsap.from("#box2", {
-    x: 1000,
-    duration:2,
-    delay:1,
-    rotate:360,
-    backgroundColor: "blue",
-    borderRadius: "50%"
-})
 
-gsap.from("h1", {
-    opacity:0,
-    color: "red",
-    duration: 2,
-    delay:1,
-    y: 30,
-    x:30,
-    stagger:1,
-})
